cc0fd20d80b26e477aa6858610beaabe3a09e367
cenkay aydin <cenkayaydin99@gmail.com>
2020-11-12 21:39:26 +0300

    create page design done

--- src/components/pagesComponents/Create.vue
+++ src/components/pagesComponents/Create.vue
@@ -5,47 +5,20 @@
     <v-form v-model="loginForm.valid" class="px-10 pt-2">
       <v-row>
         <v-col cols="6">
-          <v-text-field class="flag-TR"
-                        ref="name"
-                        v-model="name"
-                        :rules="[() => !!name || 'This field is required']"
-                        label="Baslik TR"
-                        prepend-icon="mdi-flag-checkered"
-                        placeholder="Turkce baslik giriniz"
-                        required
-                        outlined
-          ></v-text-field>
+          <v-text-field class="flag-TR" ref="name" v-model="name" :rules="[() => !!name || 'This field is required']"
+                        label="Baslik TR" prepend-icon="mdi-flag-checkered" placeholder="Turkce baslik giriniz" required
+                        outlined></v-text-field>
         </v-col>
         <v-col cols="6">
-          <v-text-field class="flag-EN"
-                        ref="name"
-                        v-model="name"
-                        :rules="[() => !!name || 'This field is required']"
-                        label="Baslik EN"
-                        prepend-icon="mdi-flag-checkered"
-                        placeholder="Ingilizce baslik giriniz"
-                        required
-                        outlined
-          ></v-text-field>
+          <v-text-field class="flag-EN" ref="name" v-model="name" :rules="[() => !!name || 'This field is required']"
+                        label="Baslik EN" prepend-icon="mdi-flag-checkered" placeholder="Ingilizce baslik giriniz"
+                        required outlined></v-text-field>
         </v-col>
         <v-col cols="6">
-          <v-combobox class="flag-TR"
-                      ref="name"
-                      v-model="model"
-                      :rules="[() => !!model || 'This field is required']"
-                      label="Keywords TR"
-                      prepend-icon="mdi-flag-checkered"
-                      placeholder="Turkce keywords giriniz"
-                      required
-                      multiple
-                      small-chips
-                      hide-selected
-                      :filter="filter"
-                      :hide-no-data="!search"
-                      :items="items"
-                      :search-input.sync="search"
-                      outlined
-          >
+          <v-combobox class="flag-TR" ref="name" v-model="model" :rules="[() => !!model || 'This field is required']"
+                      label="Keywords TR" prepend-icon="mdi-flag-checkered" placeholder="Turkce keywords giriniz"
+                      required multiple small-chips hide-selected :filter="filter" :hide-no-data="!search"
+                      :items="items" :search-input.sync="search" outlined>
             <template v-slot:no-data>
               <v-list-item>
                 <span class="subheading">Ekle</span>
@@ -75,23 +48,11 @@
           </v-combobox>
         </v-col>
         <v-col cols="6">
-          <v-combobox class="flag-EN"
-                      ref="name"
-                      v-model="modelEN"
-                      :rules="[() => !!modelEN || 'This field is required']"
-                      label="Keywords TR"
-                      prepend-icon="mdi-flag-checkered"
-                      placeholder="Turkce keywords giriniz"
-                      required
-                      multiple
-                      small-chips
-                      hide-selected
-                      :filter="filter"
-                      :hide-no-data="!search"
-                      :items="items"
-                      :search-input.sync="search"
-                      outlined
-          >
+          <v-combobox class="flag-EN" ref="name" v-model="modelEN"
+                      :rules="[() => !!modelEN || 'This field is required']" label="Keywords EN"
+                      prepend-icon="mdi-flag-checkered" placeholder="Ingilizce keywords giriniz" required multiple
+                      small-chips hide-selected :filter="filter" :hide-no-data="!search" :items="items"
+                      :search-input.sync="search" outlined>
             <template v-slot:no-data>
               <v-list-item>
                 <span class="subheading">Ekle</span>
@@ -121,53 +82,55 @@
           </v-combobox>
         </v-col>
         <v-col cols="6">
-          <v-textarea class="flag-TR"
-                      counter
-                      v-model="name"
-                      :rules="[() => !!name || 'This field is required']"
-                      label="Ozet TR"
-                      prepend-icon="mdi-flag-checkered"
-                      placeholder="Turkce ozet giriniz"
-                      required
-                      outlined
-          ></v-textarea>
+          <v-textarea class="flag-TR" counter v-model="name" :rules="[() => !!name || 'This field is required']"
+                      label="Ozet TR" prepend-icon="mdi-flag-checkered" placeholder="Turkce ozet giriniz" required
+                      outlined></v-textarea>
         </v-col>
         <v-col cols="6">
-          <v-textarea class="flag-EN"
-                      counter
-                      v-model="name"
-                      :rules="[() => !!name || 'This field is required']"
-                      label="Ozet TR"
-                      prepend-icon="mdi-flag-checkered"
-                      placeholder="Ingilizce ozet giriniz"
-                      required
-                      outlined
-          ></v-textarea>
+          <v-textarea class="flag-EN" counter v-model="name" :rules="[() => !!name || 'This field is required']"
+                      label="Ozet EN" prepend-icon="mdi-flag-checkered" placeholder="Ingilizce ozet giriniz" required
+                      outlined></v-textarea>
         </v-col>
         <v-col cols="12">
-          <v-text-field
-              ref="name"
-              v-model="name"
-              :rules="[() => !!name || 'This field is required']"
-              label="Sayfa Yonlendirme"
-              placeholder="https://"
-              required
-              outlined
-          ></v-text-field>
+          <v-text-field ref="name" v-model="name" :rules="[() => !!name || 'This field is required']"
+                        label="Sayfa Yonlendirme" placeholder="https://" required outlined></v-text-field>
         </v-col>
         <v-col cols="6">
-          <label class="custom-label flag-TR">Icerik TR &nbsp;<v-icon>mdi-flag-checkered</v-icon>
+          <label class="custom-label flag-TR">Icerik TR &nbsp;
+            <v-icon>mdi-flag-checkered</v-icon>
           </label>
           <ckeditor :editor="editor" v-model="data"></ckeditor>
         </v-col>
         <v-col cols="6">
-          <label class="custom-label flag-EN">Icerik EN &nbsp;<v-icon>mdi-flag-checkered</v-icon>
+          <label class="custom-label flag-EN">Icerik EN &nbsp;
+            <v-icon>mdi-flag-checkered</v-icon>
           </label>
           <ckeditor :editor="editor" v-model="data"></ckeditor>
         </v-col>
+        <v-col cols="12">
+          <label class="custom-label flag-EN">Resim yukle</label>
+          <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"
+                        vdropzone-removed-file="onRemoveUploadingFile"></vue-dropzone>
+        </v-col>
+        <v-col cols="6">
+          <v-select :items="itemsD" label="Sayfa Ozelligi" outlined dense></v-select>
+        </v-col>
+        <v-col cols="6">
+          <v-select :items="itemsD" label="Ust Sayfalar" outlined dense></v-select>
+        </v-col>
+        <v-col cols="6">
+          <v-row>
+            <v-col cols="6">
+              <v-checkbox v-model="checkbox1" label="Ust menude gozuksun" class="mt-0"></v-checkbox>
+            </v-col>
+            <v-col cols="6">
+              <v-checkbox v-model="checkbox2" label="Alt menude gozuksun" class="mt-0"></v-checkbox>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="6"></v-col>
         <v-col cols="2" class="px-7 py-0">
-          <v-btn class="login-btn" color="success" @click="login" :loading="loading"
-                 :disabled="loading">Kaydet
+          <v-btn class="login-btn" color="success" @click="login" :loading="loading" :disabled="loading">Kaydet
             <template v-slot:loader>
               <span>Loading...</span>
             </template>
@@ -180,9 +143,14 @@
 
 <script>
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import vue2Dropzone from 'vue2-dropzone';
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 
 export default {
   name: "Create",
+  components: {
+    vueDropzone: vue2Dropzone
+  },
   data() {
     return {
       editor: ClassicEditor,
@@ -228,9 +196,19 @@ export default {
           color: 'blue',
         },
       ],
+      itemsD: ['Foo', 'Bar', 'Fizz', 'Buzz'],
       x: 0,
       search: null,
       y: 0,
+      dropzoneOptions: {
+        url: 'https://httpbin.org/post',
+        thumbnailWidth: 150,
+        maxFilesize: 0.5,
+        addRemoveLinks: true,
+        headers: {"My-Awesome-Header": "header value"}
+      },
+      checkbox1: false,
+      checkbox2: false
     }
   },
   watch: {
